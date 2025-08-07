bc2d795c7d9627cdb90f2f0fbdbc410a780d59a6
SaharRamezani <saharramezanijolfaie@gmail.com>
2025-08-07 03:28:32 +0200

    init

--- app/src/main/java/login/WelcomeActivity.kt
+++ app/src/main/java/login/WelcomeActivity.kt
@@ -1,26 +1,6 @@
-package login
+package com.example.cyclink.login
 
-import android.content.Intent
-import android.os.Bundle
-import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
-import com.example.cyclink.R
-import com.google.firebase.Firebase
-import com.google.firebase.auth.auth
 
-class WelcomeActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+class WelcomeActivity {
 
-        if (Firebase.auth.currentUser != null) {
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }
-
-        findViewById<Button>(R.id.btn_get_started).setOnClickListener {
-            startActivity(Intent(this, LoginActivity::class.java))
-            finish()
-        }
-    }
 }
